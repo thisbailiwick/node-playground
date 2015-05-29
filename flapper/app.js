32c5cd643d3a504048305b7abaab297d1c27d574
Kevin Clark <kevin@thisbailiwick.com>
2015-05-29 07:35:16 -0700

    add home and posts state to new app.config

--- flapper/app.js
+++ flapper/app.js
@@ -41,3 +41,22 @@ app.controller('MainCtrl', [
         }
     }
 ]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider
+            .state('home',{
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+        $urlRouterProvider.otherwise('home');
+    }
+]);
