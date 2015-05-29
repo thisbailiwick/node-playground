770d455d6781c592368d28bb0c83dc778f46e808
Kevin Clark <kevin@thisbailiwick.com>
2015-05-29 14:44:44 -0700

    add commenting form and controller

--- flapper/app.js
+++ flapper/app.js
@@ -54,6 +54,16 @@ app.controller('PostsCtrl', [
     'posts',
     function($scope, $stateParams, posts){
         $scope.post = posts.posts[$stateParams.id];
+
+        $scope.addComment = function(){
+            if($scope.body == ''){reuturn;}
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
     }
 ]);
 
