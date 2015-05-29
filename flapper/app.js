b61cc97f9290022e871a3fdc43bebe943cf7c1f9
Kevin Clark <kevin@thisbailiwick.com>
2015-05-29 15:29:17 -0700

    remove mock posts

--- flapper/app.js
+++ flapper/app.js
@@ -31,14 +31,6 @@ app.controller('MainCtrl', [
         // Now any change or modification made to $scope.posts will be stored in the service and immediately accessible by any other module that injects the posts service.
         $scope.posts = posts.posts;
 
-        $scope.posts = [
-            {title: 'post 1, upvotes: 5'},
-            {title: 'post 2, upvotes: 2'},
-            {title: 'post 3, upvotes: 15'},
-            {title: 'post 4, upvotes: 9'},
-            {title: 'post 5, upvotes: 4'}
-        ]
-
         // add object into the posts array
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === ''){return;};
