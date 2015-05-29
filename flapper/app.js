6a8e8813e9142a603a13595e96dd263d63a794f5
Kevin Clark <kevin@thisbailiwick.com>
2015-05-29 15:11:15 -0700

    move app config to top of page and add 'ui.router' to list of dependencies in .module call

--- flapper/app.js
+++ flapper/app.js
@@ -1,5 +1,22 @@
-var app = angular.module('flapperNews', []);
-
+var app = angular.module('flapperNews', ['ui.router']);
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider
+            .state('home',{
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+        $urlRouterProvider.otherwise('home');
+    }
+]);
 
 app.factory('posts', [function(){
     var o = {
@@ -64,24 +81,5 @@ app.controller('PostsCtrl', [
             });
             $scope.body = '';
         };
-    }
-]);
-
-app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider){
-        $stateProvider
-            .state('home',{
-                url: '/home',
-                templateUrl: '/home.html',
-                controller: 'MainCtrl'
-            })
-            .state('posts', {
-                url: '/posts/{id}',
-                templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
-            });
-        $urlRouterProvider.otherwise('home');
     }
 ]);
