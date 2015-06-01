1afd9fd3a736b7c472e8d77d2d0378fade0651d5
Kevin Clark <kevin@thisbailiwick.com>
2015-06-01 13:16:43 -0700

    add upvote route and postschema

--- flapper/flapper-news/routes/index.js
+++ flapper/flapper-news/routes/index.js
@@ -50,3 +50,12 @@ router.param('post', function(req, res, next, id){
 router.get('/posts/:post', function(req, res){
     res.json(req.post);
 });
+
+//upvote post
+router.put('/posts/:post/upvote', function(req, res, next){
+    req.post.upvote(function(err, post){
+        if(err) {return next(err);}
+
+        res.json(post);
+    })
+});
