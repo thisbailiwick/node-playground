61491c3dd69538a6c91156c02b04a5a10733a5c8
Kevin Clark <kevin@thisbailiwick.com>
2015-05-31 22:24:12 -0700

    Pull in mongoose, Post and Comment models and add route for /posts

--- flapper/flapper-news/app.js
+++ flapper/flapper-news/app.js
@@ -62,5 +62,6 @@ module.exports = app;
 // connect to mongo
 var mongoose = require('mongoose');
 require('./models/Posts');
+require('./models/Comments');
 
 mongoose.connect('mongodb:localhost/news');
--- flapper/flapper-news/routes/index.js
+++ flapper/flapper-news/routes/index.js
@@ -7,3 +7,14 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next){
+    Post.find(function(err, posts){
+        if(err){return next(err);}
+        res.json(posts);
+    });
+});
