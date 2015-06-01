05a6c003f7ea4074aa68db447557f220e41fc4b9
Kevin Clark <kevin@thisbailiwick.com>
2015-05-31 22:44:32 -0700

    add route for pre-loading posts and route to get a single post

--- flapper/flapper-news/routes/index.js
+++ flapper/flapper-news/routes/index.js
@@ -30,3 +30,23 @@ router.post('/posts', function(req, res, next){
         res.json(post);
     });
 });
+
+//route for pre-loading posts
+router.param('post', function(req, res, next, id){
+    var query = Post.findById(id);
+
+    query.exec(function (err, post){
+        if(err) {return next(err);}
+        if(!post) {
+            return next(new Error('can\'t find post'));
+        }
+
+        req.post = post;
+        return next();
+    });
+});
+
+// return a single post
+route.get('/posts/:post', function(req, res){
+    res.json(req.post);
+});
