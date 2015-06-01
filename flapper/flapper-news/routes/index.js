80257fca0b32a28e852d580126c666cbb8b01511
Kevin Clark <kevin@thisbailiwick.com>
2015-05-31 22:25:23 -0700

    Add comment

--- flapper/flapper-news/routes/index.js
+++ flapper/flapper-news/routes/index.js
@@ -12,9 +12,12 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+//get posts
 router.get('/posts', function(req, res, next){
     Post.find(function(err, posts){
         if(err){return next(err);}
         res.json(posts);
     });
 });
+
+
