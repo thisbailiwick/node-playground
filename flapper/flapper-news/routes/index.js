dadfa0ef402cfac76c0328af8ae6e5f26fb9ad38
Kevin Clark <kevin@thisbailiwick.com>
2015-06-06 13:46:30 -0700

    Add endpoint for user authentication

--- flapper/flapper-news/routes/index.js
+++ flapper/flapper-news/routes/index.js
@@ -109,4 +109,23 @@ router.param('comment', function(req, res, next, id){
     });
 });
 
+// endpoint for authentication, returns JWT token
+router.post('/register', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err){
+        if(err){return next(err);}
+
+        return res.json({token: user.generateJWT()});
+    });
+})
+
 module.exports = router;
