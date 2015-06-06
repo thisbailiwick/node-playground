ebe8dd7016d3ea678a73126f0ae0ddf7a58be995
Kevin Clark <kevin@thisbailiwick.com>
2015-06-06 16:19:31 -0700

    add navigation controller

--- flapper/flapper-news/public/javascripts/angularApp.js
+++ flapper/flapper-news/public/javascripts/angularApp.js
@@ -220,4 +220,13 @@ angular.module('flapperNews', ['ui.router'])
             });
         };
     }
+]).
+controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth){
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
+        }
 ]);
